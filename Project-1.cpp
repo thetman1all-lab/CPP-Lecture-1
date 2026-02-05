432fdcc402e8a61bb34138d5a60bfead00f0e398
tcounihan1 <thetman1all@gmail.com>
2026-02-04 19:34:01 -0800

    Added iomanip namespace to replace clunky if statement. C handles bools natively, no extra steps

--- Project-1.cpp
+++ Project-1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main() {
 
@@ -8,18 +9,12 @@ int main() {
     int age = 24;
     double height = 1.78;
     bool excitement_condition = true;
-    std::string excitement = "false";
-
-    // If statement to handle printing true or false for the bool variable for
-    // excitement
-    if (excitement_condition) {
-        excitement = "true";
-    }
+    std::string excitement = "true";
 
     // Output declared variables in a formated sentence
     std::cout << "My name is " << name << ", I'm " << age << " years old, "
-              << height << " meters tall, and it's " << excitement
-              << " that I'm excited to learn C++." << std::endl;
+              << height << " meters tall, and it's " << std::boolalpha
+              << excitement << " that I'm excited to learn C++." << std::endl;
 
     return 0;
 }
